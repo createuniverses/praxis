433ed4a6f2226ac74fe3fb3f2f71362d16948c93
createuniverses <createuniverses@gmx.com>
2014-10-22 00:56:20 +1100

    Added a midi recorder

--- prods/Cognitive/midiinput.lua
+++ prods/Cognitive/midiinput.lua
@@ -19,14 +19,89 @@ do
 end
 
 
-recorder = {}
-function recorder.update(r)
+function initRecorder()
+  local recorder = {}
+  recorder.framenum = 0
+  recorder.notes = {}
+  recorder.currnote = 1
+  return recorder
+end
+
+recorder = initRecorder()
+
+print2(recorder.framenum)
+
+recorder.framenum = 0
+recorder.currnote = 1
+
+print2(recorder.currnote)
+print2(#recorder.notes)
+14
+
+recorder.update = recorder.record
+recorder.update = recorder.playback
+recorder.update = function (r) end
+
+function beginRecording()
+  recorder.framenum = 0
+  recorder.notes = {}
+  recorder.currnote = 1
+  recorder.update = recorder.record
+end
+
+function beginPlayback()
+  recorder.framenum = 0
+  --recorder.notes = {}
+  recorder.currnote = 1
+  recorder.update = recorder.playback
+end
+
+function stopRecorder()
+  recorder.update = function (r) end
+end
+
+function recorder.record(r)
   local midiin = {midiInputPortMessage()}
-  if #midiin == 3 then
-    if midiin[1] == 144 then -- note on
-    end
-    if midiin[1] == 128 then -- note off
-      
+  if #midiin > 0 then
+    local rec = { midi = midiin, frame = r.framenum }
+    table.insert(r.notes, rec)
+  end
+  r.framenum = r.framenum + 1
+end
+
+function recorder.playback(r)
+  if r.currnote <= #r.notes then
+    local note = r.notes[r.currnote]
+    if r.framenum == note.frame then
+      if note.midi[1] == 144 then -- note on
+        --print("Note on: ".. note.midi[2])
+        midiNoteOn(note.midi[2])
+      end
+      if note.midi[1] == 128 then -- note off
+        --print("Note off: ".. note.midi[2])
+        midiNoteOff(note.midi[2])
+      end
+      r.currnote = r.currnote + 1
     end
   end
+  r.framenum = r.framenum + 1
+end
+
+
+print2(getFunction("update"))
+function update()
+  WidgetLib.callAll("update")
+  recorder:update()
+  
+  fugue.update()
+  
+  SynthNode.updateSynthNode(sineConNode)
+  SynthNode.updateSynthNode(sineConNode2)
+  SynthNode.updateSynthNode(sineGenNode)
+  SynthNode.updateSynthNode(lpfEffNode)
+  SynthNode.updateSynthNode(sinkNode)
+  
+  g_updateCount = g_updateCount + 1
+  fugue.timeBetweenNotes = 0.1
+  
 end
