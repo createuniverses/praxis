d830d62892a220e60d9f9a171ad3ccd63bd83b2f
createuniverses <createuniverses@gmx.com>
2015-04-01 00:12:27 +1100

    Multiline editing added to the REPL server

--- prods/Cognitive/server_terminal.lua
+++ prods/Cognitive/server_terminal.lua
@@ -1,16 +1,42 @@
+-- praxis:
 
-function svrRunLuaPromptServer(sck)
-  local s = svrReceive(sck)
-  if s ~= "" then
-    luaCall(s)
-    local err = getErrorText()
-    if err ~= "" then
-      svrSend(err, sck)
-      clearError()
-    end
-    -- svrSend("Ready.\n")
-    --svrSend("\n> ")
-    svrSend("> ", sck)
-  end
+function string.at(s, i)
+  return string.char(string.byte(s,i))
 end
+
+terminalcommand = ""
+
+function svrRunLua(sck,s)
+  print = function(s)
+            if s~=nil then
+              svrSend(stripnewline(""..s).."\n", sck)
+            else
+              svrSend("nil\n", sck)
+            end
+          end
+  terminalcommand = terminalcommand .. s .. "\n"
+  local c,e = loadstring(terminalcommand)
+  if e == nil then
+    --print("running:\n" .. terminalcommand)
+    --assert(c)()
+    terminalcommand = ""
+    local r,msg = pcall(c)
+    if r==false then
+      svrSend(msg.."\n", sck)
+    end
+  else
+    local tail = string.sub(e, #e-6, #e)
+    if tail ~= "'<eof>'" then
+      --print("error for " .. terminalcommand .. "...\n")
+      terminalcommand = ""
+      svrSend(e.."\n", sck)
+      --print("\ntail was \""..tail.."\"\n")
+    else
+      --print("multiline continuing for:\n" .. terminalcommand)
+    end
+  end
+  print = print_backup
+end
+
+svrRunCode = svrRunLua
 
