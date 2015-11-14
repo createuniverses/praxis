68bfe42f85a9c52c1c8d9e47f8995388e050096f
createuniverses <createuniverses@gmx.com>
2015-11-14 16:26:09 +1100

    Syntax 2015
    Greets added

--- prods/syntax2015/prod.lua
+++ prods/syntax2015/prod.lua
@@ -66,6 +66,118 @@ end
 makeskythings()
 makeskythings()
 
+greets = [[
+TrinaryLogic
+
+Duck
+
+jazzcat
+
+Reload
+
+Voltage
+
+Mudlord
+
+cTrix
+
+jimage
+
+**************
+greetings to]]
+
+greets = [[
+jimage
+
+cTrix
+
+Mudlord
+
+Reload
+
+Voltage
+
+TrinaryLogic
+
+Duck
+
+jazzcat
+]]
+
+--namethings = {}
+--table.insert(namethings, {n = "cTrix", t = 
+names = {{n = "Greetings to"},
+         {n = "cTrix"},
+         {n = "jimage"},
+         {n = "Mudlord"},
+         {n = "Reload"},
+         {n = "Voltage"},
+         {n = "TrinaryLogic"},
+         {n = "Duck"},
+         {n = "Chicken"},
+         {n = "jazzcat"},
+         {n = "conjuror"},
+         {n = "Ript"} }
+         
+for i=1,#names,1 do
+  local name = names[i]
+  name.tpos = vec3d(0,0,i*20)
+  name.pos = cvec3d(name.tpos)
+  name.pos.y = -10
+  name.triggered = false
+end
+
+function getPlanePos()
+  local pos = vec3d(transform.getTranslation(airplane.lspace))
+  return pos
+end
+
+function renderGreets1()
+  glPushMatrix()
+  glTranslate(-30,0,100)
+  glRotate(-90,1,0,0)
+  glColor(255,255,255)
+  drawText3DStroked("GREETINGS", 0,0,0)
+  glPopMatrix()
+  
+  glPushMatrix()
+  glTranslate(-30,10,250)
+  glRotate(-30,1,0,0)
+  glColor(255,255,255)
+  drawText3DStroked(greets, 0,0,0)
+  --drawText3DStroked(greets, 0,-0.5,0)
+  --drawText3DStroked(greets, 0,-1,0)
+  glPopMatrix()
+end
+
+greetrenid = 0
+
+function renderGreets2()
+  glPushMatrix()
+  glTranslate(-30,20,100)
+  --glRotate(-90,1,0,0)
+  glColor(255,255,255)
+  
+  for i=1,#names,1 do
+    local n = names[i]
+    if n.triggered then
+      glPushMatrix()
+      glTranslate(Vector3D.getArgs(n.pos))
+      glRotate(-90,1,0,0)
+      drawText3DStroked(n.n, 0,0,0)
+      glPopMatrix()
+      n.pos.y = n.pos.y + 0.5
+    else
+      local tdist = 20
+      local diff = getPlanePos() - vec3d(n.tpos.x, 0, n.tpos.z)
+      local d = Vector3D.magnitude(diff)
+      if d < tdist then n.triggered = true end
+    end
+  end
+  
+  glPopMatrix()
+end
+
 function render()
   WidgetLib.renderAll()
 
@@ -76,6 +188,11 @@ function render()
     glVertex( 500,-2, 500)
     glVertex( 500,-2,-500)
   glEnd()]]
+  
+  -- each name appears as the plane moves forward
+  if getMp3Time() < 16 then
+    renderGreets2()
+  end
 
   glColor(150 + math.random(100),110,20)
 
@@ -221,3 +338,4 @@ setPickSphere(false)
 takeoffthrust = 0.75
 
 makeskythings()
+
