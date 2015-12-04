ddfb630da6c069a05703456834abbec94ee484ff
createuniverses <createuniverses@gmx.com>
2015-12-04 15:25:22 +1100

    Experiment

--- prods/syntax2015continued/shader.lua
+++ prods/syntax2015continued/shader.lua
@@ -15,6 +15,35 @@ void main(void)
 ]])
 end
 
+do
+shadprog2,shadres2 = glCreateProgram(
+[[
+void main(void)
+{
+  gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+}
+]],
+[[
+uniform sampler2D sampler0;
+uniform vec2 tc_offset[9];
+void main(void)
+{
+  vec4 sample[9];
+  for (int i = 0; i < 9; i++)
+  {
+    sample[i] = texture2D(sampler0,
+                          gl_TexCoord[0].st + tc_offset[i]);
+  }
+  // 1 2 1
+  // 2 1 2 / 13
+  // 1 2 1
+  gl_FragColor = (sample[0] + (2.0*sample[1]) + sample[2] +
+                 (2.0*sample[3]) + sample[4] + (2.0*sample[5]) +
+                 sample[6] + (2.0*sample[7]) + sample[8]) / 13.0;
+}
+]])
+end
+
 function render()
   WidgetLib.renderAll()
 
