84a6f53cf481ed46eb8e4fdb47a52f6bb45a1e6c
createuniverses <createuniverses@gmx.com>
2016-08-25 15:26:55 +1000

    Render for next n frames function.

--- prods/cauldron/pastime.lua
+++ prods/cauldron/pastime.lua
@@ -6,8 +6,20 @@ function dofornext(fn, ntimes)
     fn()
     c = c + 1
     if c >= ntimes then
-      updatefns["timedtask"] = nil
+      updatefns.timedtask = nil
     end
   end
   updatefns.timedtask = ufn
+end
+
+function renderfornext(fn, ntimes)
+  local c = 0
+  local ufn = function ()
+    fn()
+    c = c + 1
+    if c >= ntimes then
+      renderfns.timedtask = nil
+    end
+  end
+  renderfns.timedtask = ufn
 end
